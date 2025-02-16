0bf18d2c19af609e4451b3e0cbf3dc100c2b622d
William <williamjamesjanohan1997@gmail.com>
2025-02-16 20:58:06 +0800

    1215_Vid12_pivot tables and belongs to many relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,21 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-
-class Job extends Model{
+class Job extends Model {
     use HasFactory;
-    protected $table = "job_listings";
+
+    protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
 
     public function employer()
     {
         return $this->belongsTo(Employer::class);
-
     }
 
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+
+    }
 }
