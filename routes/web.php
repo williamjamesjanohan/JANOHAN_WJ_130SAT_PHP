876fb48546908290d2ff27ac9fce88fc5d22011a
William <williamjamesjanohan1997@gmail.com>
2025-02-16 21:34:41 +0800

    0222_Vid16_forms and csrf explained with examples

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,42 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
+
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
-
     ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+
+    return view('jobs.show', ['job' => $job]);
 });
+
+Route::post('/jobs', function () {
+    // validation...
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
+});
+
 Route::get('/contact', function () {
     return view('contact');
 });
