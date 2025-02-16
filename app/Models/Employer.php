842ec7470630f9a25a709d1c2692033e5b877608
William <williamjamesjanohan1997@gmail.com>
2025-02-16 20:36:16 +0800

    0215_Model Factories

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,13 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
-
-class Job extends Model{
+class Employer extends Model
+{
     use HasFactory;
-    protected $table = "job_listings";
-
-    protected $fillable = ['title', 'salary'];
-
 }
--- database/migrations/0001_01_01_115110_create_job_listings_table.php
+++ database/migrations/0001_01_01_115110_create_job_listings_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Employer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,8 @@ return new class extends Migration
     {
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
+           // $table->unsignedBigInteger('employer_id');
+            $table->foreignIdFor(Employer::class);
             $table->string('title');
             $table->string('salary');
             $table->timestamps();
