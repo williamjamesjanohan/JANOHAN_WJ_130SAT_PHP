02fd83978980c0aedc42e58609ba09c618d605d7
William <williamjamesjanohan1997@gmail.com>
2025-02-16 21:07:43 +0800

    0215_Vid13_eager loading and the n-plus-1 problem

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,23 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
-
 Route::get('/', function () {
-
-
-   return view('home');
+    return view('home');
 });
 
 Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->get();
+
     return view('jobs', [
         'jobs' => Job::all()
+
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-
     return view('job', ['job' => $job]);
 });
-
 Route::get('/contact', function () {
     return view('contact');
 });
